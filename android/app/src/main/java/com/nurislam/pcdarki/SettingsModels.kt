486622384faee9c1ec444f9b3d0ab22353d0fcee
nur12islam <68411946+nur12islam@users.noreply.github.com>
2026-09-12 03:05:07 +0530

    app: add settings sections model

---
+++ android/app/src/main/java/com/nurislam/pcdarki/SettingsModels.kt
@@ -0,0 +1,8 @@
+package com.nurislam.pcdarki
+
+enum class SettingsSection(val title: String) {
+    ACCOUNT("Account"),
+    APPEARANCE("Appearance"),
+    DISPLAY("Display"),
+    ABOUT("About PC-DARKI")
+}
