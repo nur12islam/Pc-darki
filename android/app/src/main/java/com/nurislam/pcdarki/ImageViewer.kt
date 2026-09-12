6b5e32049cb0a635e3e0303c2cbb64cc528b8dc5
nur12islam <68411946+nur12islam@users.noreply.github.com>
2026-09-12 22:53:18 +0530

    Fix Coil 3 import

--- android/app/src/main/java/com/nurislam/pcdarki/ImageViewer.kt
+++ android/app/src/main/java/com/nurislam/pcdarki/ImageViewer.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 
 @Composable
 fun PCDarkiImageViewer() {
