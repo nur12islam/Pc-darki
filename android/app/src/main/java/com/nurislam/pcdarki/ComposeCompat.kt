e165500fe06efb667c38a99e3c58f826d355eb5e
nur12islam <68411946+nur12islam@users.noreply.github.com>
2026-09-12 23:45:27 +0530

    Fix Compose clickable compatibility import

--- android/app/src/main/java/com/nurislam/pcdarki/ComposeCompat.kt
+++ android/app/src/main/java/com/nurislam/pcdarki/ComposeCompat.kt
@@ -1,5 +1,6 @@
 package com.nurislam.pcdarki
 
+import androidx.compose.foundation.clickable
 import androidx.compose.ui.Modifier
 
-internal fun Modifier.pcdarkiClickable(onClick: () -> Unit): Modifier = androidx.compose.foundation.clickable(onClick = onClick)
+internal fun Modifier.pcdarkiClickable(onClick: () -> Unit): Modifier = clickable(onClick = onClick)
